f52bc11435ee9b028b5e929d2fc4bf81718aa40c
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-07 21:42:10 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Session5_Assignment
+﻿using System.Drawing;
+
+namespace Session5_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1- . Write a program that prints an identity matrix using for loop, in other
+            //  words takes a value n from the user and shows the identity table of size n *n.
+
+            Console.Write("Enter identity matrix size :  ");
+             int.TryParse(Console.ReadLine(),out int x);
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (j == i) { Console.Write(1+" "); }
+                    else { Console.Write(0 + " "); }
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
         }
     }
 }
